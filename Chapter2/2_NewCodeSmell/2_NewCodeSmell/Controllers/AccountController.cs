532e6b2e51587f253939d1ae065200796b3192b3
nlivaic <nlivaic@gmail.com>
2017-06-22 16:23:39 +0200

    Type: feat
    Initial commit.
    Controller is tightly coupled to the SecurityService class.

---
+++ Chapter2/2_NewCodeSmell/2_NewCodeSmell/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using _2_NewCodeSmell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_NewCodeSmell.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SecurityService securityService;
+
+        public AccountController() {
+            this.securityService = new SecurityService();
+        }
+
+        [HttpGet]
+        public ActionResult ChangePassword() {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ChangePassword(int userId, string newPassword)
+        {
+            return View();
+        }
+    }
+}
