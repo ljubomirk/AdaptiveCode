e100419f02ed161505aa792bd76a76e8ea6702b4
nlivaic <nlivaic@gmail.com>
2017-06-22 15:23:40 +0200

    Type: feat
    Observe how MessagePrinter.dll is not loaded, even though it is referenced by the 1_AssemblyDependency project.
    If you delete MessagePrinter.dll from output folder and run the app again, it will still work.

--- Chapter2/1_AssemblyDependency/1_AssemblyDependency/Program.cs
+++ Chapter2/1_AssemblyDependency/1_AssemblyDependency/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessagePrinter;
 
 namespace _1_AssemblyDependency {
     class Program {
         static void Main(string[] args) {
+            Console.ReadKey();
         }
     }
 }
