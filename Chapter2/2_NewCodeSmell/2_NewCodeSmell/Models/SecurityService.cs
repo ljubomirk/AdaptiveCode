d6893965e7e8d51309f72778ffc0efc61e806fb2
nlivaic <nlivaic@gmail.com>
2017-06-22 23:09:17 +0200

    Type: refactor
    Throw exception for empty parameter.

--- Chapter2/2_NewCodeSmell/2_NewCodeSmell/Models/SecurityService.cs
+++ Chapter2/2_NewCodeSmell/2_NewCodeSmell/Models/SecurityService.cs
@@ -1,11 +1,12 @@
-﻿using _2_NewCodeSmell.Infrastructure;
-using _2_NewCodeSmell.Interfaces;
+﻿using _2_NewCodeSmell.Interfaces;
+using System;
 
 namespace _2_NewCodeSmell.Models {
     public class SecurityService {
         private IUserRepository userRepository;
 
         public SecurityService(IUserRepository userRepository) {
+            if (userRepository == null) throw new ArgumentNullException("userRepository");
             this.userRepository = userRepository;
             // this.Session = SessionFactory.GetSession();
         }
