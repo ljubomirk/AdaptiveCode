03c8b5e3d01fccd51ef22c49e5f8fa2076bf53eb
nlivaic <nlivaic@gmail.com>
2017-06-25 20:19:10 +0200

    Type: chore
    Dependency resolver - Ninject.

---
+++ Chapter2/2_NewCodeSmell/2_NewCodeSmell/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using _2_NewCodeSmell.Interfaces;
+using _2_NewCodeSmell.Models;
+using Ninject;
+using Ninject.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace _2_NewCodeSmell.Infrastructure {
+    public class NinjectDependencyResolver : IDependencyResolver {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver() {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+
+        public IBindingToSyntax<T> Bind<T>() {
+            return kernel.Bind<T>();
+        }
+
+        public void AddBindings() {
+            Bind<IUserRepository>().To<UserRepository>();
+            Bind<ISecurityService>().To<SecurityService>();
+        }
+    }
+}
